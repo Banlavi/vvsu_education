a8aac6822ea81859a3b264fd929da9eb2cda1307
Banlavi <valerabyrs88@gmail.com>
2021-10-01 09:35:34 +1000

    Dev 1.21

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 a = 1
+print(type(a))
+b=1.1
+print(type(b))
+
+c='c'
+print(type(c))
+
+o, p="hi", "g"
+f=False
+print(type(f))
+
+g = a+b
+print(type(g)) 
+
+print(ord("/"))
+
+s = input()
+sl=len(s)
+if sl == 1: 
+    if ord(s[0]) == 47:
+        print("command list")
+    else: print('inc_code')
+else: print('tadjik')
