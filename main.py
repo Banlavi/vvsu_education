a397b353957f1df738eb4c73eaddf8f7d926a034
Banlavi <valerabyrs88@gmail.com>
2021-10-01 10:05:17 +1000

    dev 1.3

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import math
+
 a = 1
 print(type(a))
-b=1.1
+b = 1.1
 print(type(b))
 
-c='c'
+c = 'c'
 print(type(c))
-import math
+
 print(math.sqrt(100))
 print(10 + 10)
 print(10 - 10)
@@ -14,19 +16,31 @@ print(10 // 10)
 print(10 * 10)
 print(10 ** 10)
 print(10 % 10)
-o, p="hi", "g"
-f=False
+o, p = "hi", "g"
+f = False
 print(type(f))
+
+lst = [1, 2, 78, 78, "wer", '12']
+print(type(lst))
+y = len(lst)
+print(y)
+for i in range(len(lst)): 
+    print(lst[i]) 
+
 
 g = a+b
 print(type(g)) 
 
 print(ord("/"))
-
+'''
 s = input()
-sl=len(s)
+sl = len(s)
 if sl == 1: 
-    if ord(s[0]) == 47:
+    if ord(s[0]) == 47: 
         print("command list")
-    else: print('inc_code')
-else: print('tadjik')
+    else: 
+        print('inc_code')
+else: 
+    print('tadjik')
+# pep 8
+'''
